ee27e316c846e808c69deb808580a81858f90e99
Kevin Lee <kevvin851222@gmail.com>
2021-01-26 11:59:16 +0800

    加入controller test

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+
+        public ActionResult Test()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
